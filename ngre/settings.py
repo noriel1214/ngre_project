7753b1759fde1ca91dc09b9c173de2942bf202e8
Krista Galang <kristagg21@Kristas-MacBook-Air.local>
2019-07-29 20:45:25 -0400

    req.txt

--- ngre/settings.py
+++ ngre/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'ngre/static')
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'ngre/static'),
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
